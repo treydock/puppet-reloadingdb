60937131fc628dbe1295157ce34fa323dff9aa17
Trey Dockendorf <treydock@gmail.com>
2019-04-01 17:15:23 -0400

    Send mail through gandi

--- site_modules/profile/manifests/postfix.pp
+++ site_modules/profile/manifests/postfix.pp
@@ -1,7 +1,24 @@
 #
 class profile::postfix (
   Hash $configs = {},
+  String $username = 'changeme',
+  String $password = 'changeme',
 ) {
+
+  package { ['cyrus-sasl','cyrus-sasl-lib','cyrus-sasl-plain']:
+    ensure => 'installed',
+    before => Class['::postfix'],
+  }
+
   include ::postfix
   create_resources('postfix::config', $configs)
+
+  postfix::hash { '/etc/postfix/sasl_passwd':
+    ensure  => 'present',
+    content => "${::postfix::relayhost} ${username}:${password}\n"
+  }
+  postfix::hash { '/etc/postfix/generic':
+    ensure  => 'present',
+    content => "@${::postfix::myorigin} no-reply+reloadingdb@${::postfix::myorigin}\n",
+  }
 }
